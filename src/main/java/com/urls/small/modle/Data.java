4249cc32cf7d2b7a5bad0b7d66ff32c62bd4fc59
YI <ilovey_hwy@163.com>
2018-11-02 15:10:19 +0800

    :wrench: 优化代码

--- src/main/java/com/urls/small/modle/Data.java
+++ src/main/java/com/urls/small/modle/Data.java
@@ -12,9 +12,9 @@ public class Data {
      * code	        String	    success	                                            上传文件状态。正常情况为 success。出现错误时为 error
      * filename	    String	    smms.jpg	                                        上传文件时所用的文件名
      * storename	String	    561cc4e3631b1.png	                                上传后的文件名
-     * size	        Int	187851	文件大小
-     * width	    Int	1157	图片的宽度
-     * height	    Int	680	    图片的高度
+     * size	        Int	187851	                                                    文件大小
+     * width	    Int	1157	                                                    图片的宽度
+     * height	    Int	680	                                                        图片的高度
      * hash	        String	    nLbCw63NheaiJp1	                                    随机字符串，用于删除文件
      * delete	    String	    https://sm.ms/api/delete/nLbCw63NheaiJp1	        删除上传的图片文件专有链接
      * url	        String	    https://ooo.0o0.ooo/2015/10/13/561cfc3282a13.png	图片服务器地址
